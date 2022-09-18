40dcdcbb411a4cfa4c331a710a1c177a55936e04
MSWEIMZ <99701537@qq.com>
2022-09-18 15:56:48 +0800

    可以运行

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public  App(){
-        System.out.println("Hello class！！！");
+    public static void main(String[] args){
+            System.out.println("Hello class！！！");
+
     }
+
 }
